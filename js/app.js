dd9fbe62d17f19c4d024084a11ac86049acec386
Erick <erick.developer002@gmail.com>
2021-02-14 16:07:35 -0500

    fin del capitulo

---
+++ js/app.js
@@ -0,0 +1,20 @@
+import nuevaFuncion, { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from './cliente.js';
+import { Empresa } from './empresa.js';
+
+nuevaFuncion();
+
+console.log(nombreCliente);
+
+console.log(ahorro);
+
+console.log(mostrarInformacion(nombreCliente, ahorro));
+
+tieneSaldo(ahorro);
+
+const cliente = new Cliente(nombreCliente, ahorro);
+
+console.log(cliente.mostrarInformacion());
+
+// Importar empresa
+const empresa = new Empresa('Codigo Con Juan', 100, 'Aprendizaje en Línea');
+console.log(empresa.mostrarInformacion());
